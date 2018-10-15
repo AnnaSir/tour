39510dc2aa49df018535ca2d1ad0e4c0bf1ad398
AnnaSir <marsmy@rambler.ru>
2018-10-16 00:58:05 +0300

    дизайн заголовков

--- src/main/webapp/static/filter2.js
+++ src/main/webapp/static/filter2.js
@@ -7,32 +7,14 @@ var prevDD = {
 var currFilter;
 var prevFilters = [];
 
-
 var Filter = {
 
 
     createDD: function () {
+
         var content = this;
-        // //event.stopPropagation();
-        // var a = $(content).css('text-decoration');
-        // var isColumnFiltered = $(content).css('text-decoration') === 'underline solid rgb(0, 0, 0)';
-
-
         var currHead = $(content).attr('data-columnName');
-
-        // var breakHere = false;
-        //
-        // prevDD.forEach(function (item, i, arr) {
-        //     if (item.ddHeader===currHead) {
-        //         //getLastDropDown(currHead);
-        //         breakHere=true;
-        //     }
-        // });
-        //
-        // if (breakHere) return;
-
         if (currHead === currFilter) return; //если был применен фильтр, то не изменяем содержимое окна
-
 
         var finList = [];
         var menu = $(content).siblings(".dropdown-menu");
@@ -41,13 +23,6 @@ var Filter = {
 
             var currObj = prevDD[currHead];
             finList = Object.keys(currObj);
-
-            // menu.find("input").each(
-            //     function () {
-            //         if (currObj.indexOf($(this).attr("data-value")) >= 0)
-            //             $(this).prop('checked', false);
-            //     }
-            // )
 
         } else {
 
@@ -73,39 +48,18 @@ var Filter = {
             )
         }
 
-
-    },
-
-
-    getLastDropDown: function (currHead) {
-
-        // var currObj = prevDD[currHead];
-        //
-        // return Mustache.to_html($("#ddPattern").html(), {data: currObj, column: currHead});
-
     },
 
 
     getFilterResult: function () {
-        // var ddButton = $('.dropdown').find('.ddButton');
-        // ddButton.each(
-        //     function () {
-        //         ddButton.css('text-decoration', 'none');
-        //         var g = ddButton.css('text-decoration');
-        //     });
-
 
         var content = this;
         var menu = $(content).closest(".dropdown-menu");
-        var columnName = menu.parent().find('button').attr('data-columnName')
-
-        //var a = menu.find("input:checkbox:not(:checked)");
+        var columnName = menu.parent().find('button').attr('data-columnName');
         var allInputs = menu.find("input");
         var ckeckedInputs = menu.find("input:checked");
         var isAllChecked = allInputs.length === ckeckedInputs.length;
 
-
-        //columnProp[currColumn]= menu.parent().find('button').attr('data-columnName');
         currFilter = columnName;
         if (prevFilters.indexOf(currFilter) === -1) {
             prevFilters.push(currFilter);
@@ -114,11 +68,6 @@ var Filter = {
 
         allInputs.each(
             function () {
-                // var filteredList = {
-                //     ddHeader: $(this).attr('data-columnName'),
-                //     value: $(this).attr('data-value'),
-                //     checked: $(this).prop('checked')
-                // };
                 if (!isAllChecked) {
                     columnProp[$(this).attr('data-value')] = $(this).prop('checked');
                 }
@@ -129,11 +78,9 @@ var Filter = {
                 var row = $(cell).parent();
                 if ($(this).prop('checked')) {
                     row.show();
-                    //prevDD.splice(prevDD.indexOf($(this).attr("data-value")), 1);
                 }
                 if (!$(this).prop('checked')) {
                     row.hide();
-                    //prevDD.push($(this).attr('data-value'));
                 }
 
             }
@@ -142,7 +89,7 @@ var Filter = {
 
         if (isAllChecked) { //если нет пустых checkbox
             var a = prevFilters.splice(-1, 1);
-            //currFilter = prevFilters[prevFilters.length - 1];
+            menu.parent().find('button').css("text-decoration", "none");
             return;
         }
 
@@ -152,85 +99,3 @@ var Filter = {
 
     }
 };
-
-
-/*var prevDD = [];
-
-
-var Filter = {
-
-
-    createDD: function () {
-        var content = this;
-        //event.stopPropagation();
-        var a = $(content).css('text-decoration');
-        var isColumnFiltered = $(content).css('text-decoration') === 'underline solid rgb(0, 0, 0)';
-        var currHead = $(content).attr('data-columnName');
-        var listForDD = $('[data-columnName =' + currHead + '][data-atr=ddBody]');
-        var finList = [];
-        listForDD.each(function () {
-                var cellValue = $(this).attr('data-value');
-                var row = $('td[data-value=' + cellValue + ']').parent();
-                if (finList.indexOf(cellValue) >= 0 || (!row.is(':visible') && !isColumnFiltered)) return;
-
-                finList.push(cellValue);
-            }
-        );
-
-        var dDHtml = Mustache.to_html($("#ddPattern").html(), {data: finList, column: currHead});
-
-        var menu = $(content).siblings(".dropdown-menu");
-        menu.html(dDHtml);
-        menu.find("input").each(
-            function () {
-                if (prevDD.indexOf($(this).attr("data-value")) >= 0)
-                    $(this).prop('checked', false);
-            }
-        )
-
-
-    },
-
-
-    getFilterResult: function () {
-        var r = $('.dropdown').find('.ddButton');
-        r.each(
-            function () {
-                r.css('text-decoration','none');
-                var g = r.css('text-decoration');
-            });
-
-
-        var content = this;
-        var menu = $(content).closest(".dropdown-menu");
-
-        //var a = menu.find("input:checkbox:not(:checked)");
-        var a = menu.find("input");
-        a.each(
-            function () {
-                // if ($(this).text()==="Выделить все"){
-                //     a.each(function () {
-                //         $(this).prop('checked',true)
-                //     })
-                // }
-                var cell = $("#tourTable").find("td[data-columnName=" + $(this).attr("data-columnName") + "][data-value=" + $(this).attr("data-value") + "]");
-                var row = $(cell).parent();
-                if ($(this).prop('checked')&&!row.is(':visible')) {
-                    row.show();
-                    var r = $(this).attr("data-value");
-                    var v = prevDD.indexOf($(this).attr("data-value"));
-                    var c = prevDD.splice(prevDD.indexOf($(this).attr("data-value")),1);
-                }
-                if (!$(this).prop('checked')&&row.is(':visible')) {
-                    row.hide();
-                    prevDD.push($(this).attr('data-value'));
-                }
-
-            }
-        );
-        var c = menu.parent();
-        menu.parent().find('button').css("text-decoration", "underline");
-
-
-    }
-};*/
